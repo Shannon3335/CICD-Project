4e3d1832efd88b31098d45aaed99d4c3260156dc
G00398070 <G00398070@atu.ie>
2022-11-11 13:59:12 +0000

    Created project with springboot framework

---
+++ src/main/java/ie/atu/project/CiCdGroupProject/CiCdGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.project.CiCdGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdGroupProjectApplication.class, args);
+	}
+
+}
